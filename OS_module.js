b19678d44f2d23eafd147beb322db5928fbad1c1
brain hamrage <ayajansari2002@gmail.com>
2024-09-18 16:02:37 +0530

    added OS module

---
+++ OS_module.js
@@ -0,0 +1,9 @@
+
+import os from "os"
+
+console.log("home directory : ",os.homedir());
+console.log("uptime : ",os.uptime());
+console.log("cpus  : ",os.cpus());
+console.log("os release : ",os.release());
+console.log("total memory : ",os.totalmem());   //total amount of memory in system in bytes
+console.log("home directory : ",os.type());
